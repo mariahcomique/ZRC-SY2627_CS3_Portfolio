7abe140bbc3f20fea613971b5099ac64964a0c89
mariahcomique <mbcomique@zrc.pshs.edu.ph>
2026-09-25 05:24:51 +0800

    Add files via upload

---
+++ q1/sg5/classHero.py
@@ -0,0 +1,16 @@
+class Hero:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+    def take_damage(self, amount):
+        self.hp -= amount
+
+arthur = Hero("Arthur", 100)
+morgana = Hero("Morgana", 100)
+
+
+arthur.take_damage(10)
+
+print(f"{arthur.name} HP: {arthur.hp}")  
+print(f"{morgana.name} HP: {morgana.hp}") 
